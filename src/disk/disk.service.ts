9943a44b04edf0df95efb2bddcf27d38ac09db22
Biplob <biplob.ruet15@gmail.com>
2022-09-23 15:12:04 +0600

    add dependency of power in disk module

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+    constructor(private powerSupply: PowerService){}
+
+    getData() {
+        console.log('giving 20 watt...')
+        this.powerSupply.supplyPower(20)
+        return 'data'
+    }
+}
