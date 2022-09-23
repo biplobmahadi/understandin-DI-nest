8c6dcb5a72d562831647a0b52675aff5f32927bb
Biplob <biplob.ruet15@gmail.com>
2022-09-23 15:10:09 +0600

    update cpu module with power dependency

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
     constructor(private powerService: PowerService) {}
+
+    compute(a: number, b: number) {
+        console.log('using 15 watt...')
+        this.powerService.supplyPower(15)
+        return a + b;
+    }
 }
